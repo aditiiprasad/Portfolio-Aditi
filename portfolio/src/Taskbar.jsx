a5b9542d7cee47916634ce9b686cc23ce4fce6c9
aditiiprasad <aditi03prasad@gmail.com>
2024-11-08 15:01:07 +0530

    taskbar

--- portfolio/src/Taskbar.jsx
+++ portfolio/src/Taskbar.jsx
@@ -3,6 +3,12 @@
 import React from 'react';
 import './Taskbar.css';  // We will create Taskbar.css for styling
 import { FaLinkedin, FaGithub, FaEnvelope, FaCode } from 'react-icons/fa';
+import mailicon from "./assets/mailicon.png"
+import instaicon from "./assets/instaicon.png"
+import linkedinicon from "./assets/linkedinicon.png"
+import githubicon from "./assets/githubicon.png"
+import leetcode from "./assets/leetcode.png"
+
 
 function Taskbar() {
   // Handler for downloading resume (example link, replace with actual path)
@@ -30,24 +36,33 @@ function Taskbar() {
 
       <div className="social-media-links">
         {/* LinkedIn */}
-        <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
-          <FaLinkedin className="icon" />
+        <a href="https://www.linkedin.com/in/aditiiprasad" target="_blank" rel="noopener noreferrer">
+        <img src={linkedinicon} alt="" />
         </a>
 
         {/* GitHub */}
-        <a href="https://github.com/your-github-profile" target="_blank" rel="noopener noreferrer">
-          <FaGithub className="icon" />
+        <a href="https://github.com/aditiiprasad" target="_blank" rel="noopener noreferrer">
+        <img src={githubicon} alt="" />
         </a>
 
         {/* LeetCode */}
-        <a href="https://leetcode.com/your-leetcode-profile" target="_blank" rel="noopener noreferrer">
-          <FaCode className="icon" />
+
+        <a href="https://leetcode.com/aditiiprasad" target="_blank" rel="noopener noreferrer">
+        <img src={leetcode} alt="" />
         </a>
 
         {/* Email */}
-        <a href="mailto:your-email@example.com">
-          <FaEnvelope className="icon" />
+        
+        <a href="mailto:aditi03prasad@gmail.com">
+          <img src={mailicon} alt="" />
         </a>
+
+        {/* instagram */}
+        
+        <a href="https://instagram.com/aditiiprasad">
+          <img src={instaicon} alt="" />
+        </a>
+
       </div>
 
 
