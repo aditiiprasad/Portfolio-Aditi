a02d6ecc9b1317e659cab80b958b590ed8253b3c
aditiiprasad <aditi03prasad@gmail.com>
2024-11-24 14:35:07 +0530

    Education section

--- portfolio/src/components/Edu.jsx
+++ portfolio/src/components/Edu.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Edu.css';
 import busimg from './images/edu.png'
+import bit from './images/bit.png'
+import kv from './images/kv.png'
 
 const Edu = () => {
   return (
@@ -9,7 +11,28 @@ const Edu = () => {
       <img src={busimg} className="busimg" alt="edu" />
      
       </div>
-    </div>
+      <div className='bitinfo'>
+      <div className='bit'>
+    <p className="college-name">Birla Institute of Technology, Mesra</p>
+    <p className="year">2023-2027</p>
+    <p className="degree">Btech in Computer Science</p>
+      </div>
+        <div className='bitimg'>
+        <img src={bit} className="biti" alt="bit" />
+        </div>
+      </div>
+      <div className='bitinfo'>
+     
+        <div className='bitimg'>
+        <img src={kv} className="biti" alt="bit" />
+        </div>
+        <div className='bit'>
+    <p className="college-name">Kendriya Vidyalaya</p>
+   
+    <p className="degree">PCMB</p>
+      </div>
+      </div>
+   </div>
   )
 }
 
